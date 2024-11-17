b37c1e27f67bda8922716d64833227693a7de9a1
Kate <rita15062425@icloud.com>
2024-11-17 19:03:48 +0000

    Getters and setters

--- Group_37/src/main/java/uk/ac/ucl/comp0010/model/Student.java
+++ Group_37/src/main/java/uk/ac/ucl/comp0010/model/Student.java
@@ -55,8 +55,58 @@ public class Student {
   public void registerModule(Module m) {
     if (!modules.contains(m)) {
       modules.add(m);
-      m.setRegistredStudent(this);
+      m.setRegisteredStudent(this);
     }
   }
+
+  // Get/Setters
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public List<Module> getRegisteredModules() {
+    return modules;
+  }
+
+  public void setRegisteredModules(List<Module> modules) {
+    this.modules = modules;
+  }
+
 
 }
