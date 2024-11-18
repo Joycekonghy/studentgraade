fc96354a075e0a55627beacd00049d4cfc454500
zceivsm <zceivsm@ucl.ac.uk>
2024-11-18 00:40:23 +0000

    grade class

---
+++ Group_37/src/main/java/uk/ac/ucl/comp0010/model/Grade.java
@@ -0,0 +1,40 @@
+package uk.ac.ucl.comp0010.model;
+
+public class Grade {
+    
+    private Student student;
+    private int score;
+    private Module module;
+    
+    public Grade(Student student, int score, Module module) {
+        this.student = student;
+        this.score = score;
+        this.module = module;
+    }
+    
+
+    public Module getModule() {
+        return module;
+    }
+
+    public void setModule(Module module) {
+        this.module =  module;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student =  student;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score =  score;
+    }
+    
+}
