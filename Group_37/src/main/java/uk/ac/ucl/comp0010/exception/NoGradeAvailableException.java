5caef7537e105843a3144e5978c946850c726397
Joyce Kong <joyce_kong@Joyces-Laptop.local>
2024-11-18 02:11:26 +0000

    added NoGradeAvailable Exception and NoRegistrationException

---
+++ Group_37/src/main/java/uk/ac/ucl/comp0010/exception/NoGradeAvailableException.java
@@ -0,0 +1,9 @@
+package uk.ac.ucl.comp0010.exception;
+
+public class NoGradeAvailableException extends Exception{
+
+  public NoGradeAvailableException(String message) {
+    super(message);
+  }
+  
+}
---
+++ Group_37/src/main/java/uk/ac/ucl/comp0010/exception/NoRegistrationException.java
@@ -0,0 +1,9 @@
+package uk.ac.ucl.comp0010.exception;
+
+public class NoRegistrationException extends Exception{
+
+    public NoRegistrationException(String message) {
+        super(message);
+    }
+  
+}
