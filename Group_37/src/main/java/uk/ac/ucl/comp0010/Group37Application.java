19ea0e810bfbcfa927af397acc5b7a646707b137
Joyce Kong <joyce_kong@eduroam-int-dhcp-97-139-104.ucl.ac.uk>
2024-11-19 16:19:16 +0000

    networkConnection

--- Group_37/src/main/java/uk/ac/ucl/comp0010/Group37Application.java
+++ Group_37/src/main/java/uk/ac/ucl/comp0010/Group37Application.java
@@ -9,7 +9,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Group37Application {
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(Group37Application.class);
-        app.setDefaultProperties(Collections.singletonMap("server.port", "2800"));
         app.run(args);
     }
 }
